edda65a3a87b85962e5a039baddf563d5f0fe5be
Huseinmuffin04 <21523004@students.uii.ac.id>
2023-05-24 17:24:13 +0700

    Create veriflaporan.blade.php

---
+++ resources/views/veriflaporan.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>UKM Insight</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div>
+        <!-- breadcrumb -->
+        <div class="mb-4 mt-4 pl-32">
+            <nav class="w-full rounded-md">
+                <ol class="list-reset flex">
+                    <li>
+                        <a href="#" class="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700">Home</a>
+                    </li>
+                    <li>
+                        <span class="mx-2 text-neutral-500">></span>
+                    </li>
+                    <li class="text-neutral-500">Verifikasi Laporan Kegiatan</li>
+                </ol>
+            </nav>
+        </div>
+        <!-- sidebar -->
+        <div class="container mx-auto max-w-6xl grid h-screen place-items-center">
+            <div class="mb-4 flex bg-gray-300">
+                <div class="w-64 bg-gray-300">
+                    <ul class="py-4">
+                        <li class="mb-2 px-6 py-3 bg-gray-400 rounded-xl hover:bg-gray-600">
+                            <a href="#" class="font-medium text-black-400 rounded-lg hover:text-white">Kegiatan 1</a>
+                        </li>
+                        <li class="mb-2 px-6 py-3 bg-gray-400 rounded-xl hover:bg-gray-600">
+                            <a href="#" class="font-medium text-black-400 hover:text-white">Kegiatan 2</a>
+                            <span class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-red-600 bg-white rounded-full cursor-pointer">2</span>
+                        </li>
+                        <li class="mb-2 px-6 py-3 bg-gray-400 rounded-xl hover:bg-gray-600">
+                            <a href="#" class="font-medium text-black-400 hover:text-white">Kegiatan 3</a>
+                        </li>
+                        <li class="mb-2 px-6 py-3 bg-gray-400 rounded-xl hover:bg-gray-600">
+                            <a href="#" class="font-medium text-black-400 hover:text-white">Kegiatan 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Tengah -->
+                <div class="flex-1 bg-gray-100">
+                    <h1 class="text-4xl font-bold py-8 px-16">Rincian Verifikasi Pelaporan Kegiatan</h1>
+                    <div class="container mx-auto px-6 sm:flex sm:flex-wrap sm:gap-6 sm:justify-evenly">
+                        <div class="rounded-md shadow-lg overflow-hidden mb-10 bg-white sm:mb-0 sm:w-64 md:w-80 group relative hover:scale-95 transition-all duration-700">
+                            <img src="https://source.unsplash.com/600x400?sport" alt="imegcap" class="w-full" />
+                            <div class="px-6 py-6">
+                                <div class="font-bold text-xl mb-2 text-slate-700">Rincian Laporan Kegiatan</div>
+                                <p class="text-sm text-slate-600">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, fugiat voluptatibus voluptas ab saepe asperiores, iusto placeat maiores harum eligendi doloremque magni quae delectus totam? A alias molestiae fugit
+                                    quam.
+                                </p>
+                            </div>
+                            <button class="my-10 bg-sky-400 px-5 py-2 rounded-full text-white font-semibold font-mono block mx-auto hover:bg-sky-600 active:bg-sky-800 focus:ring focus:ring-sky-200 dark:bg-slate-300 dark:text-slate-800 dark:hover:text-slate-900 dark:hover:bg-slate-100">
+                                Lihat Laporan
+                            </button>
+                        </div>
+                        <div class="rounded-md shadow-lg overflow-hidden mb-10 bg-white sm:mb-0 sm:w-64 md:w-80 group relative hover:scale-95 transition-all duration-700">
+                            <img src="https://source.unsplash.com/600x400?photography" alt="imegcap" class="w-full" />
+                            <div class="px-6 py-6">
+                                <div class="font-bold text-xl mb-2 text-slate-700">Rincian Laporan Kegiatan</div>
+                                <p class="text-sm text-slate-600">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, fugiat voluptatibus voluptas ab saepe asperiores, iusto placeat maiores harum eligendi doloremque magni quae delectus totam? A alias molestiae fugit
+                                    quam.
+                                </p>
+                            </div>
+                            <button class="my-10 bg-sky-400 px-5 py-2 rounded-full text-white font-semibold font-mono block mx-auto hover:bg-sky-600 active:bg-sky-800 focus:ring focus:ring-sky-200 dark:bg-slate-300 dark:text-slate-800 dark:hover:text-slate-900 dark:hover:bg-slate-100">
+                                Lihat Laporan
+                            </button>
+                        </div>
+                        <div class="rounded-md shadow-lg overflow-hidden mb-10 bg-white sm:mb-0 sm:w-64 md:w-80 group relative hover:scale-95 transition-all duration-700">
+                            <img src="https://source.unsplash.com/600x400?robot" alt="imegcap" class="w-full" />
+                            <div class="px-6 py-6">
+                                <div class="font-bold text-xl mb-2 text-slate-700">Rincian Laporan Kegiatan</div>
+                                <p class="text-sm text-slate-600">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, fugiat voluptatibus voluptas ab saepe asperiores, iusto placeat maiores harum eligendi doloremque magni quae delectus totam? A alias molestiae fugit
+                                    quam.
+                                </p>
+                            </div>
+                            <button class="my-10 bg-sky-400 px-5 py-2 rounded-full text-white font-semibold font-mono block mx-auto hover:bg-sky-600 active:bg-sky-800 focus:ring focus:ring-sky-200 dark:bg-slate-300 dark:text-slate-800 dark:hover:text-slate-900 dark:hover:bg-slate-100">
+                                Lihat Laporan
+                            </button>
+                        </div>
+                        <div class="rounded-md shadow-lg overflow-hidden mb-10 bg-white sm:mb-0 sm:w-64 md:w-80 group relative hover:scale-95 transition-all duration-700">
+                            <img src="https://source.unsplash.com/600x400?sing" alt="imegcap" class="w-full" />
+                            <div class="px-6 py-6">
+                                <div class="font-bold text-xl mb-2 text-slate-700">Rincian Laporan Kegiatan</div>
+                                <p class="text-sm text-slate-600">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, fugiat voluptatibus voluptas ab saepe asperiores, iusto placeat maiores harum eligendi doloremque magni quae delectus totam? A alias molestiae fugit
+                                    quam.
+                                </p>
+                            </div>
+                            <button class="my-10 bg-sky-400 px-5 py-2 rounded-full text-white font-semibold font-mono block mx-auto hover:bg-sky-600 active:bg-sky-800 focus:ring focus:ring-sky-200 dark:bg-slate-300 dark:text-slate-800 dark:hover:text-slate-900 dark:hover:bg-slate-100">
+                                Lihat Laporan
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
